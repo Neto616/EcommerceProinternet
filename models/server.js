9868be98e64340cf9c83f9e076acbc80985cbde9
Spyker1 <alexrdz1221@gmail.com>
2023-08-04 22:09:16 -0600

    Update server.js

--- models/server.js
+++ models/server.js
@@ -4,60 +4,61 @@ const nodemailer = require('nodemailer');
 const hbs = require('nodemailer-express-handlebars');
 
 class Server {
-    constructor(){
-        
-        this.app = express();
-        this.port = process.env.PORT;
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-        this.middleawares();
-        this.routes();
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    middleawares(){
-        this.app.use( express.static('public'));
-        this.app.use(cors()); // Agrega el middleware de CORS para permitir solicitudes desde otros dominios
+  middlewares() {
+    this.app.use(express.static('public'));
+    this.app.use(express.json());
+    this.app.use(cors()); // Agrega el middleware de CORS para permitir solicitudes desde otros dominios
+  }
 
-        // Configuración del transporte de correo electrónico
-        const transporter = nodemailer.createTransport({
-            service: "Gmail",  
-            auth: {
-                user: "netoilluminati258@gmail.com",
-                pass: process.env.PASSWORD // Acceder a la contraseña desde las variables de entorno
-            },
-        });
+  routes() {
+    // Configuración del transporte de correo electrónico
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: 'netoilluminati258@gmail.com',
+        pass: process.env.PASSWORD, // Acceder a la contraseña desde las variables de entorno
+      },
+    });
 
-        // Definir correos electrónicos en copia
-        const multiples = ['basn160603@gmail.com', 'leonardo.cantulara@hotmail.com'];
+    this.app.post('/enviarCorreo', (req, res) => {
+      // Obtener los datos del formulario del cliente
+      const { nombreCompleto, correoElectronico, informacion } = req.body;
 
-        // Configurar el objeto de correo
-        const mail = {
-            from: 'netoilluminati258@gmail.com',
-            to: 'alexrdz1221@gmail.com',
-            cc: multiples.join(', '), // Agrega los correos en copia como una cadena separada por comas
-            subject: 'Correo prueba 4',
-            text: 'Creo que ya quedo',
-            html: "<b>Holaaaa ✔</b>",
-        };
+      // Configurar el objeto de correo
+      const mailOptions = {
+        from: 'netoilluminati258@gmail.com',
+        to: 'alexrdz1221@gmail.com',
+        cc: 'basn160603@gmail.com, leonardo.cantulara@hotmail.com',
+        subject: 'Formulario de contacto - EcommerceProInternet',
+        text: `Nombre completo: ${nombreCompleto}\nCorreo electrónico: ${correoElectronico}\nInformación: ${informacion}`,
+      };
 
-        // Enviar el correo cuando se inicie el servidor
-        transporter.sendMail(mail, function(error, info){
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email sent: ' + info.response);
-            }
-        });
-    }
+      // Enviar el correo electrónico
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ message: 'Ocurrió un error al enviar el correo.' });
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.json({ message: 'El correo se envió correctamente.' });
+        }
+      });
+    });
+  }
 
-    routes(){
-
-    }
-
-    listen(){
-        this.app.listen(this.port, () => {
-            console.log (`Link del servidor http://localhost:${this.port}`);
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Link del servidor http://localhost:${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
