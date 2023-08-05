38c474c0f004c442d867b0c42be749220fc4e955
Neto616 <140271677+Neto616@users.noreply.github.com>
2023-08-05 10:50:32 -0600

    Add files via upload

--- models/server.js
+++ models/server.js
@@ -1,29 +1,51 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
 
 class Server {
-    constructor(){
-        
-        this.app = express();
-        this.port = process.env.PORT;
+  constructor() {
+    this.app = express();
 
-        this.middleawares();
-    }
+    this.port = process.env.PORT;
 
-    middleawares(){
-        this.app.use( express.static('public'))
-    }
+    this.sesiones();
+    this.middleawares();
+    this.routes();
+  }
 
-    routes(){
+  middleawares() {
+    // this.app.use( express.static('public') );
+  }
 
-    }
+  sesiones() {
+    this.app.use(
+      session({
+        secret: "keyboard cat", //Clave
+        resave: true, //Forma de almacenamiento
+        saveUninitialized: true,
+        cookie: { maxAge: 60000 },
+      })
+    );
+  }
 
-    listen(){
-        this.app.listen(this.port, () => {
-            console.log (`Link del servidor http://localhost:${this.port}`)
-        })
-    }
+  routes() {
+    // this.app.use(cors());
+    this.app.get("/", (req, res) => {
+      req.session.usuario = {
+        nombre: "Nestor Ivan",
+        rol: "Admin",
+      };
 
+      req.session.visitas = req.session.visitas ? ++req.session.visitas : 1;
+      res.send(`El usuario: ${req.session.usuario.nombre}\ncon rol: ${req.session.usuario.rol}\nha visitado esta pagina ${req.session.visitas} veces`);
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor funcionando aqui: http://localhost:${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
