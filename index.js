54ed49c3ef7ce9014092fe9e7a4ff20849383f54
Spyker1 <alexrdz1221@gmail.com>
2023-08-04 21:03:56 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-require ('dotenv').config();
+require('dotenv').config();
 
 const Server = require('./models/server');
 const server = new Server();
 
+const express = require("express");
+const nodemailer = require('nodemailer');
+const hbs = require('nodemailer-express-handlebars');
+const app = express();
+
+const transporter = nodemailer.createTransport({
+    service: "Gmail",  
+    auth: {
+        user: "netoilluminati258@gmail.com",
+        pass: process.env.PASSWORD // Acceder a la contraseña desde las variables de entorno
+    },
+});
+
+const multiples = [
+    'basn160603@gmail.com',
+    'leonardo.cantulara@hotmail.com'
+];
+
+const mail = {
+    from: 'netoilluminati258@gmail.com',
+    to: 'alexrdz1221@gmail.com',
+    cc: multiples.join(', '), // Agrega los correos en copia como una cadena separada por comas
+    subject: 'Correo prueba 4',
+    text: 'Creo que ya quedo',
+    html: "<b>Holaaaa ✔</b>",
+};
+
+transporter.sendMail(mail, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
+
 server.listen();
 
-console.log("Si jala el link perres")
+console.log("Si jala el link perres");
